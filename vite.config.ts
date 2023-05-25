1b0b9907b2e153230614473a2f5b4ab8ccf62ec5
stLmpp <gui.stlmpp@gmail.com>
2023-05-25 20:46:02 -0300

    remove purge css TODO

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,18 @@ import { spawnSync } from 'child_process';
 import { defineConfig, type PluginOption } from 'vite';
 
 function sassWatch(): PluginOption {
-  async function buildCss() {
+  function buildCss() {
     spawnSync('npm', ['run', 'compile-css'], { shell: true });
-    // const purge = await new PurgeCSS().purge({
-    // TODO make this work https://github.dev/carbon-design-system/carbon-preprocess-svelte/tree/main/src
-    //   content: ['src/**.{html,svelte}'],
-    //   css: ['static/global.css'],
-    // });
-    // await writeFile('static/global.css', purge[0].css);
   }
 
   return {
     name: 'sass-watch',
     buildStart: async () => {
-      await buildCss();
+      buildCss();
     },
     handleHotUpdate: async ({ file, server }) => {
       if (/\.scss$/.test(file)) {
-        await buildCss();
+        buildCss();
         server.ws.send({
           type: 'full-reload',
           path: '*',
